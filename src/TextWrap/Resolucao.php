843b523299d591682aebad37592c28c26d516e17
guipernicone <guipernicone@hotmail.com>
2019-01-22 06:12:22 -0200

    Arrumando erro de string vazia e retirando comentarios

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -18,103 +18,90 @@ class Resolucao implements TextWrapInterface {
 
     /*Separa cada palavra da string fornecida em uma posicao do array teste, removendo todos espaços */
     $palavras = explode(" ",$text);
-    if($text != ""){
       /*Verifica se o texto já foi completamente formatdo */
-      foreach($palavras as $palavra){
+    foreach($palavras as $palavra){
 
-        $lenPalavra = strlen($palavra);
-    
-        /*Verifica se a palavra é maior que o limite da linha */
-        if($lenPalavra >$length){
-          
-      
-            /*Verifica se o cursor ja nao esta no fim de uma linha */
-            if($posLinha == $length){
-              $posLinha = 0;
-              $linha++;
-            }
-            else{/*Verifica se é preciso espaço antes da palavra ou nao */
-              if($posLinha != 0){/*Verifica se o cursao nao esta no inicio de uma linha */
-
-                $posLinha++;/*Simula um espaço inserido */
-
-                /*Verifica se o cursor esta no fim de uma linha */
-                if($posLinha == $length){
-                  $posLinha = 0;
-                  $linha++;
-                }
-                else{
-                  $resultado[$linha] .= " ";/*Adiciona  um espaço*/
-                }
-            }
-            }
-            /*Realiza o while enquanto a palavra nao tenha sido completamente 
-            * copiada ao array resultado 
-            */
-            $j = 0;
-            while($lenPalavra > $j){
-            
-              /*Verifica se o index do array ja existe*/
-              if(array_key_exists($linha, $resultado) == false){
-                $resultado[$linha] = $palavra[$j];
-              }
-              else{
-                $resultado[$linha] .= $palavra[$j];
-              }
-
-              $j++;
-              $posLinha++;
-
-              /*Verifica se chegou ao limite da linha*/
-              if($posLinha == $length){
-                $posLinha = 0;
-                $linha++;
-              }
-            }
-            /*Verifica se após a palavra existira espaço ou nao */
-            /*if($posLinha != 0 && $posLinha < $length){
-              $resultado[$linha] .= " ";
-              $posLinha++;
-            }*/
-        }
-        else{/*Caso a palavra é menor que o limite da linha */
-
-          if($posLinha != 0 && $posLinha != $length){
-            $posLinha++;
-          }
-          if($lenPalavra <= ($length-$posLinha)){/*Caso a palavra coube dentro do limite da linha */
-
-            /*Verifica se o index do array ja existe*/
-            if(array_key_exists($linha, $resultado) == false){
-              $resultado[$linha] = $palavra;
-            }
-            else{
-              $resultado[$linha] .= " ";
-              $resultado[$linha] .= $palavra;
-            }
-          
-            $posLinha += $lenPalavra;
+      $lenPalavra = strlen($palavra);
+  
+      /*Verifica se a palavra é maior que o limite da linha */
+      if($lenPalavra >$length){
         
-          }
-          else{/*Caso a palavra nao coube dentro do limite da linha */
-            $posLinha = 0;
-            $linha++;
-
-            $resultado[$linha] = $palavra;
-            $posLinha += $lenPalavra;
-          }
-
-          /*Verifica se chegou ao limite da linha*/
+    
+          /*Verifica se o cursor ja nao esta no fim de uma linha */
           if($posLinha == $length){
             $posLinha = 0;
             $linha++;
           }
+          else{/*Verifica se é preciso espaço antes da palavra ou nao */
+            if($posLinha != 0){/*Verifica se o cursao nao esta no inicio de uma linha */
+
+              $posLinha++;/*Simula um espaço inserido */
+
+              /*Verifica se o cursor esta no fim de uma linha */
+              if($posLinha == $length){
+                $posLinha = 0;
+                $linha++;
+              }
+              else{
+                $resultado[$linha] .= " ";/*Adiciona  um espaço*/
+              }
+          }
+          }
+          /*Realiza o while enquanto a palavra nao tenha sido completamente 
+          * copiada ao array resultado 
+          */
+          $j = 0;
+          while($lenPalavra > $j){
           
-          /*Verifica se após a palavra existira espaço ou nao */
-          /*if($posLinha != 0 && $posLinha < $length){
-            $resultado[$linha] .= " ";
+            /*Verifica se o index do array ja existe*/
+            if(array_key_exists($linha, $resultado) == false){
+              $resultado[$linha] = $palavra[$j];
+            }
+            else{
+              $resultado[$linha] .= $palavra[$j];
+            }
+
+            $j++;
             $posLinha++;
-          }*/
+
+            /*Verifica se chegou ao limite da linha*/
+            if($posLinha == $length){
+              $posLinha = 0;
+              $linha++;
+            }
+          }
+      }
+      else{/*Caso a palavra é menor que o limite da linha */
+
+        if($posLinha != 0 && $posLinha != $length){
+          $posLinha++;
+        }
+        if($lenPalavra <= ($length-$posLinha)){/*Caso a palavra coube dentro do limite da linha */
+
+          /*Verifica se o index do array ja existe*/
+          if(array_key_exists($linha, $resultado) == false){
+            $resultado[$linha] = $palavra;
+          }
+          else{
+            $resultado[$linha] .= " ";
+            $resultado[$linha] .= $palavra;
+          }
+        
+          $posLinha += $lenPalavra;
+      
+        }
+        else{/*Caso a palavra nao coube dentro do limite da linha */
+          $posLinha = 0;
+          $linha++;
+
+          $resultado[$linha] = $palavra;
+          $posLinha += $lenPalavra;
+        }
+
+        /*Verifica se chegou ao limite da linha*/
+        if($posLinha == $length){
+          $posLinha = 0;
+          $linha++;
         }
       }
     }
