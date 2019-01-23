28c6549f5f1356bcdcba8e76662103c2bbe20edf
guipernicone <guipernicone@hotmail.com>
2019-01-23 03:41:56 -0200

    Adicionado testes para palavras grandes e textos longos

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -111,13 +111,15 @@ class Resolucao implements TextWrapInterface {
 /*
 $teste = new Resolucao();
 
-$texto = "Se vi mais longe foi por estar de pé sobre ombros de gigantes";
+//$texto = "Se vi mais longe foi por estar de pé sobre ombros de gigantes";
 
 //$texto = "12345678 9";
-
+//$texto = "Um teste para palavras maiores que o limite da linha 12345678 como horizontalmente";
 //$texto = "";
 //$texto = "O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.";
-$limiteLinha = 8;
+$texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing";
+
+$limiteLinha = 30;
 
 $res = $teste->textWrap($texto,$limiteLinha);
 
@@ -125,6 +127,5 @@ echo("Resultado:</br>");
 foreach($res as $print){
   print_r($print);
   echo("</br>");
-}*/
-?>
-
+}
+*/
--- tests/src/Unit/TextWrapTest.php
+++ tests/src/Unit/TextWrapTest.php
@@ -18,6 +18,8 @@ class TextWrapTest extends TestCase {
   public function setUp() {
     $this->resolucao = new Resolucao();
     $this->baseString = "Se vi mais longe foi por estar de pé sobre ombros de gigantes";
+    $this->baseString2 = "Um teste para palavras maiores que o limite da linha 12345678 como horizontalmente";
+    $this->baseString3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing";
   }
 
   /**
@@ -65,6 +67,76 @@ class TextWrapTest extends TestCase {
     $this->assertEquals("pé sobre", $ret[3]);
     $this->assertEquals("ombros de", $ret[4]);
     $this->assertEquals("gigantes", $ret[5]);
+  }
+
+   /**
+   * Testa a quebra de linha para palavras longas.
+   *
+   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
+   */
+  public function testForBigWords() {
+    $ret = $this->resolucao->textWrap($this->baseString2, 6);
+    $this->assertCount(16, $ret);
+    $this->assertEquals("Um", $ret[0]);
+    $this->assertEquals("teste", $ret[1]);
+    $this->assertEquals("para p", $ret[2]);
+    $this->assertEquals("alavra", $ret[3]);
+    $this->assertEquals("s maio", $ret[4]);
+    $this->assertEquals("res", $ret[5]);
+    $this->assertEquals("que o", $ret[6]);
+    $this->assertEquals("limite", $ret[7]);
+    $this->assertEquals("da", $ret[8]);
+    $this->assertEquals("linha", $ret[9]);
+    $this->assertEquals("123456", $ret[10]);
+    $this->assertEquals("78", $ret[11]);
+    $this->assertEquals("como h", $ret[12]);
+    $this->assertEquals("orizon", $ret[13]);
+    $this->assertEquals("talmen", $ret[14]);
+    $this->assertEquals("te", $ret[15]);
+  }
+
+   /**
+   * Testa a quebra de linha para palavras longas.
+   *
+   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
+   */
+  public function testForBigWords2() {
+    $ret = $this->resolucao->textWrap($this->baseString2, 10);
+    $this->assertCount(10, $ret);
+    $this->assertEquals("Um teste", $ret[0]);
+    $this->assertEquals("para", $ret[1]);
+    $this->assertEquals("palavras", $ret[2]);
+    $this->assertEquals("maiores", $ret[3]);
+    $this->assertEquals("que o", $ret[4]);
+    $this->assertEquals("limite da", $ret[5]);
+    $this->assertEquals("linha", $ret[6]);
+    $this->assertEquals("12345678", $ret[7]);
+    $this->assertEquals("como horiz", $ret[8]);
+    $this->assertEquals("ontalmente", $ret[9]);
+  }
+
+   /**
+   * Testa a quebra de linha para textos longos.
+   *
+   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
+   */
+  
+  public function testForLongTexts() {
+    $ret = $this->resolucao->textWrap($this->baseString3, 30);
+    $this->assertCount(13, $ret);
+    $this->assertEquals("Lorem ipsum dolor sit amet,", $ret[0]);
+    $this->assertEquals("consectetur adipiscing elit.", $ret[1]);
+    $this->assertEquals("Etiam eget ligula eu lectus", $ret[2]);
+    $this->assertEquals("lobortis condimentum. Aliquam", $ret[3]);
+    $this->assertEquals("nonummy auctor massa.", $ret[4]);
+    $this->assertEquals("Pellentesque habitant morbi", $ret[5]);
+    $this->assertEquals("tristique senectus et netus et", $ret[6]);
+    $this->assertEquals("malesuada fames ac turpis", $ret[7]);
+    $this->assertEquals("egestas. Nulla at risus.", $ret[8]);
+    $this->assertEquals("Quisque purus magna, auctor", $ret[9]);
+    $this->assertEquals("et, sagittis ac, posuere eu,", $ret[10]);
+    $this->assertEquals("lectus. Nam mattis, felis ut", $ret[11]);
+    $this->assertEquals("adipiscing", $ret[12]);
   }
 
 }
